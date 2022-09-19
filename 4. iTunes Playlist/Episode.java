4a29fe07592eeba4e12f0ee5d2e1844afb505408
Jonathan Zhang <112275815+jonathanzhang-cs@users.noreply.github.com>
2022-09-19 01:38:02 -0400

    Create Episode.java

---
+++ 4. iTunes Playlist/Episode.java
@@ -0,0 +1,31 @@
+class Episode
+{
+    private String title;
+    private double length;
+
+    public Episode next;
+    public Episode prev;
+
+    public Episode(String title, double length, Episode next, Episode prev)
+    {
+        this.title = title;
+        this.length = length;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public double getLength()
+    {
+        return this.length;
+    }
+
+    public String toString()
+    {
+        return "("+ this.title + "|" + this.length + "MIN)" ;
+    }
+ }
